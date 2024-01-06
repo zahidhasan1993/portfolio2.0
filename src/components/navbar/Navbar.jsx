3e5eda4bfd3f9acfaa071dfc33e2602a4fc4fa74
devZahid <officialzahidhasan19932021@gmail.com>
2024-01-07 00:38:47 +0600

    adding navbar and design with sass

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,34 @@
 import "./navbar.scss";
 
-
 const Navbar = () => {
-    return (
+  return (
+    <div className="navbar">
+      {/* side menu */}
+      <div className="wrapper">
         <div>
-            hello
+          <span>Zahid Hasan</span>
         </div>
-    );
+        <div className="links">
+          <a href="">
+            <img src="/public/facebook.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/public/github.png" alt="" />
+          </a>
+          <a href="">
+            {" "}
+            <img
+              src="/public/linkedin-logo-linkedin-icon-transparent-free-png.webp"
+              alt=""
+            />
+          </a>
+          <a href="">
+            <img src="/public/youtube.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
