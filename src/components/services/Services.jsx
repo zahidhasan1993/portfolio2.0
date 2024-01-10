3797a50fe68e0a031b21a7412644b9a9a43595c0
devZahid <officialzahidhasan19932021@gmail.com>
2024-01-11 02:22:42 +0600

    completing service section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -26,7 +26,40 @@ const Services = () => {
           <button>WHAT I DO?</button>
         </div>
       </motion.div>
-      <motion.div className="cardContainer"></motion.div>
+      <motion.div className="cardContainer">
+        <div className="card">
+          <h1>Single-page applications</h1>
+          <p>
+            I can build your website in one page website.You can make your dream
+            to be real and i am going to make this happen.
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="card">
+          <h1>Dashboards</h1>
+          <p>
+            For you bigger project where you need a dashboard to track on your
+            website i can do this for you.
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="card">
+          <h1>E-commerce</h1>
+          <p>
+            I am offering a full e-commerce platform from scratch. Your need is
+            command for me.
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="card">
+          <h1>Dynamic Website</h1>
+          <p>
+            I am offering a full stack dynamic website. It is a challenge for
+            me. With MERN Stack i can build your dream projects.
+          </p>
+          <button>Go</button>
+        </div>
+      </motion.div>
     </motion.div>
   );
 };
