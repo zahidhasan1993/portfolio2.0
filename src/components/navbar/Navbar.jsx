15b845b4440455b1012c46b6410e89aaee323530
devZahid <officialzahidhasan19932021@gmail.com>
2024-01-11 12:57:49 +0600

    services section completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,35 @@
 import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <div className="navbar">
       <Sidebar></Sidebar>
       <div className="wrapper">
         <div>
-          <motion.span initial={{opacity: 0.2, scale: 0.5}} animate={{opacity: 1, scale: 1}} transition={{duration: 1, ease:"easeIn", delay: 0.3}}>Zahid Hasan</motion.span>
+          <motion.span
+            initial={{ opacity: 0.2, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, ease: "easeIn", delay: 0.3 }}
+          >
+            Zahid Hasan
+          </motion.span>
         </div>
         <div className="links">
-          <a href="">
+          <a href="https://www.facebook.com/profile.php?id=100066076810758">
             <img src="/public/facebook.png" alt="" />
           </a>
-          <a href="">
+          <a href="https://github.com/zahidhasan1993">
             <img src="/public/github.png" alt="" />
           </a>
-          <a href="">
+          <a href="https://www.linkedin.com/in/zahid-hasan-2434a0279/">
             {" "}
             <img
               src="/public/linkedin-logo-linkedin-icon-transparent-free-png.webp"
               alt=""
             />
           </a>
-          <a href="">
+          <a href="https://www.youtube.com/">
             <img src="/public/youtube.png" alt="" />
           </a>
         </div>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,14 @@
 import "./service.scss";
-import { motion } from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 
 const Services = () => {
   return (
-    <motion.div className="service">
+    <motion.div
+      className="service"
+      initial={{ opacity: 0, scale: 0.5, x: 500 }}
+      whileInView={{ opacity: 1, scale: 1, x: 0 }}
+      transition={{ duration: 1 }}
+    >
       <motion.div className="textContainer">
         <p>
           🚀 Code your success, Grow
@@ -11,54 +16,77 @@ const Services = () => {
         </p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div
+        className="titleContainer"
+        initial={{ opacity: 0, scale: 0.5, y: 500 }}
+        whileInView={{ opacity: 1, scale: 1, y: 1 }}
+        transition={{ duration: 1, delay: 1, ease: easeInOut }}
+      >
         <div className="title">
           <img src="/public/people.webp" alt="" />
           <h1>
-            <b>Unique</b> Ideas
+            <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className="title">
           {" "}
           <h1>
-            <b>For Your</b> Business.
+            <motion.b whileHover={{ color: "orange" }}>For Your</motion.b>{" "}
+            Business.
           </h1>
           <button>WHAT I DO?</button>
         </div>
       </motion.div>
-      <motion.div className="cardContainer">
-        <div className="card">
+      <motion.div
+        className="cardContainer"
+        initial={{ opacity: 0, scale: 0.5, y: -500 }}
+        whileInView={{ opacity: 1, scale: 1, y: 1 }}
+        transition={{ duration: 1, delay: 1, staggerChildren: 0.3 }}
+      >
+        <motion.div
+          className="card"
+          whileHover={{ backgroundColor: "lightgray", color: "black" }}
+        >
           <h1>Single-page applications</h1>
           <p>
             I can build your website in one page website.You can make your dream
             to be real and i am going to make this happen.
           </p>
           <button>Go</button>
-        </div>
-        <div className="card">
+        </motion.div>
+        <motion.div
+          className="card"
+          whileHover={{ backgroundColor: "lightgray", color: "black" }}
+        >
           <h1>Dashboards</h1>
           <p>
             For you bigger project where you need a dashboard to track on your
             website i can do this for you.
           </p>
           <button>Go</button>
-        </div>
-        <div className="card">
+        </motion.div>
+        <motion.div
+          className="card"
+          whileHover={{ backgroundColor: "lightgray", color: "black" }}
+        >
           <h1>E-commerce</h1>
           <p>
             I am offering a full e-commerce platform from scratch. Your need is
             command for me.
           </p>
           <button>Go</button>
-        </div>
-        <div className="card">
+        </motion.div>
+        <motion.div
+          className="card"
+          whileHover={{ backgroundColor: "lightgray", color: "black" }}
+        >
           <h1>Dynamic Website</h1>
           <p>
             I am offering a full stack dynamic website. It is a challenge for
             me. With MERN Stack i can build your dream projects.
           </p>
           <button>Go</button>
-        </div>
+        </motion.div>
       </motion.div>
     </motion.div>
   );
