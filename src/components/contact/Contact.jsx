ca9ce4590327238fe65c787ab720d1c4c19e1f10
devZahid <officialzahidhasan19932021@gmail.com>
2024-01-15 00:30:27 +0600

    contact section completed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,47 @@
 import "./contact.scss";
+import { easeInOut, motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 
 const Contact = () => {
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_htz5037",
+        "template_9zxk4fq",
+        form.current,
+        "KEUMi8Yt0TDvV0fIF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setError(false);
+          setSuccess(true);
+        },
+        (error) => {
+          setError(true);
+          setSuccess(false);
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <div className="contact">
-      <div className="textContainer">
-        <h1>Let&apos;<span>s work</span> together</h1>
+    <motion.div className="contact">
+      <motion.div
+        className="textContainer"
+        initial={{ opacity: 0, scale: 0.5, x: -300 }}
+        whileInView={{ opacity: 1, scale: 1, x: 1 }}
+        transition={{ duration: 1, delay: 0.2, ease: easeInOut }}
+      >
+        <h1>
+          Let&apos;<span>s work</span> together
+        </h1>
         <div className="item">
           <h2>Mail</h2>
           <span>zahidhasan19932023@gmail.com</span>
@@ -17,16 +54,23 @@ const Contact = () => {
           <h2>Address</h2>
           <span>145 /- Bagbari, Uttarpara, Dhaka-1216</span>
         </div>
-      </div>
-      <div className="formContainer">
-        <form>
-          <input type="text" placeholder="Name" required />
-          <input type="email" placeholder="Email" required />
-          <textarea rows={8} placeholder="Message"></textarea>
-          <button>Submit</button>
+      </motion.div>
+      <motion.div
+        className="formContainer"
+        initial={{ opacity: 0, scale: 0.5, x: 300 }}
+        whileInView={{ opacity: 1, scale: 1, x: 1 }}
+        transition={{ duration: 1, delay: 0.2, ease: easeInOut }}
+      >
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <textarea rows={8} name="message" placeholder="Message"></textarea>
+          <button type="submit">Submit</button>
+          {error && "Error"}
+          {success && "Success"}
         </form>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
